05d66c8ab435f4429f90733bb60c3c96aebc9a46
Sebastian <sebastiansloma@gmail.com>
2023-08-27 12:21:28 +0200

    .

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,6 +6,11 @@ function NavBar() {
     <div>
         <div>
             <Image src={'/logo.jpg'} alt={'logo'} width={120} height={60} />
+            <div>
+                <h2>Home</h2>
+                <h2>History</h2>
+                <h2>Help</h2>
+            </div>
         </div>
     </div>
   )
